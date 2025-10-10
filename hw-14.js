1d5c91ef07561ac0b72e8b9b317617bce0d40c87
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2025-10-10 18:59:04 +0300

    123

--- hw-14.js
+++ hw-14.js
@@ -189,7 +189,7 @@ console.log(getUsersWithEyeColor(users, 'blue'));// [об'єкт Moore Hensley, 
 
 // Отримати масив імен користувачів за статтю (поле gender)
 const getUsersWithGender = (users, gender) => {
- return users.filter(user => user.gender === gender)
+ return users.filter(user => user.gender === gender).map(user => user.name)
 };
 console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
